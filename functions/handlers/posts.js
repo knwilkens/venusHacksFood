d8419e04c936f4c058191195fe9b20230d8e19ef
Kailey <kwilkens@ucsd.edu>
2021-04-25 07:20:08 -0700

    posts can be completed, and other things

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -1,10 +1,12 @@
 const { db } = require('../util/admin')
 const postsDB = db.collection('posts');
 
-
+//get all posts, for reference see index.js
 exports.getAllPosts = (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
   postsDB
-    //.orderBy('createdAt', 'desc')  
+    //.orderBy('createdAt', 'desc')
     .get()
     .then((allPosts) => {
       let posts = [];
@@ -16,16 +18,77 @@ exports.getAllPosts = (req, res) => {
           status: doc.data().status
         });
       });
-      res.render('index', {posts: posts});
+      res.json(posts);
     })
     .catch(err => {
-      res.status(500).json({error: 'could not retrieve posts...'});
+      res.status(500).json({error: err});
     });
 }
 
+//get posts by limit, for reference see index.js
+exports.getPosts = (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  postsDB
+  .limit(parseInt(req.params.limit))
+  .get()
+  .then((allPosts) => {
+    let posts = [];
+    allPosts.forEach(doc => {
+      posts.push({
+        id: doc.id,
+        postUser: doc.data().postUser,
+        title: doc.data().title,
+        description: doc.data().description,
+        status: doc.data().status
+      });
+    });
+    res.json(posts);
+  })
+  .catch(err => {
+    res.status(500).json({error: err});
+  });
+}
+
+exports.updatePostStatusById = (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  let toUpdate = postsDB.doc(req.params.id);
+  let posts = [];
+  toUpdate.get()
+    .then((x) => {
+      let newReciever = (x.data().receiveUser != "") ? "" : req.params.user;
+      let newStatus = (x.data().status != "Complete") ? "Complete" : "Incomplete";
+      toUpdate.update({ "receiveUser" : newReciever})
+      .then((x) => {
+        toUpdate.update({ "status" : newStatus })
+        .then((x) => {
+          toUpdate.get()
+          .then((x) => {
+            res.json({"status": x.data().status});
+          })
+        })
+      })
+    })
+    
+  
+}
+
+//get single post by id, for reference see index.js
+exports.getPostById = (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+
+
+}
+
+
+// Make a single post and send to DB
+// endpoint --> http://localhost:5001/venushacksfood/us-central1/api/post
 exports.makeOnePost = (req, res) => {
-  console.log(req.body);
-  //console.log(localStorage.getItem('user'));
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+
   const newPost = {
     postUser: req.body.postUser,
     receiveUser: '',
--- functions/index.js
+++ functions/index.js
@@ -9,21 +9,44 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const cors = require('cors')({origin: true});
+
 // front end link
 app.set('view engine', 'ejs');
 app.set('views', '../public'); // for render 
 
-// handlers
-const { getAllPosts, makeOnePost } = require('./handlers/posts');
+// Router
+const { getAllPosts, makeOnePost, getPosts, updatePostStatusById } = require('./handlers/posts');
 
 app.get('/home', getAllPosts);
+
+
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname+'/../public/log-in.html'));
 })
-app.get('/view-posts', getAllPosts);
+
+
+//API to request all posts
+// endpoint --> http://localhost:5001/venushacksfood/us-central1/api/view-all-posts
+app.get('/view-all-posts', getAllPosts);
+
+// Requests posts where limit equals the number of posts requested
+// endpoint --> http://localhost:5001/venushacksfood/us-central1/api/view-posts/:limit
+// example --> /view-posts/10 --> returns 10 posts
+app.get('/view-posts/:limit', getPosts);
+
+// Requests posts where limit equals the number of posts requested
+// endpoint --> http://localhost:5001/venushacksfood/us-central1/api/update-status-by-id/:id
+// example --> /update-status-by-id/128SDAWhas18A --> updates status of post
+app.get('/update-status-by-id/:id/:user', updatePostStatusById);
+
+// Landpage for making a single post
 app.get('/post', (req, res) => {
   res.render('make-post');
 })
+
+// Make a single post and send to DB
+// endpoint --> http://localhost:5001/venushacksfood/us-central1/api/post
 app.post('/post', makeOnePost);
 
 // // Create and Deploy Your First Cloud Functions
